72237265bd1343e5577bb8f60c30eafaffdcadc8
moritzebeling <git@moritzebeling.com>
2020-04-13 17:28:26 +0200

    post item in posts list

--- site/snippets/channel/posts.php
+++ site/snippets/channel/posts.php
@@ -14,7 +14,17 @@ $layout = isset( $layout ) ? $layout : 'cards';
 						<?= $image->figure() ?>
 					<?php endif; ?>
 
-					<?= $post->title(); ?>
+					<span class="date"><?= $post->date(); ?></span>
+
+					<h3 class="title"><?= $post->title(); ?></h3>
+
+					<?php if( $post->subtitle()->isNotEmpty() ): ?>
+						<h4 class="subtitle"><?= $post->subtitle(); ?></h4>
+					<?php endif; ?>
+
+					<?php if( $post->type()->isNotEmpty() ): ?>
+						<span class="type"><?= $post->type(); ?></span>
+					<?php endif; ?>
 
 				</a>
 			</li>
