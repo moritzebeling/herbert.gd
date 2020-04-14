9f8f6327cbfc6132b7df99abfa844ae0debc2f34
moritzebeling <git@moritzebeling.com>
2020-04-14 23:23:59 +0200

    layout footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,26 +1,17 @@
 	<footer class="site-footer">
-		<div class="grid">
 
-			<div class="col-6">
+		&copy; <?= date('Y') ?>
 
-				&copy; <?= date('Y') ?>
+		<a href="<?= $site->url() ?>"><?= $site->title() ?></a>
 
-				<a href="<?= $site->url() ?>"><?= $site->title() ?></a>
+		<?php if( $site->imprint()->isNotEmpty() ): ?>
+			<a target="_blank" href="<?= $site->imprint() ?>">Imprint</a>
+		<?php endif; ?>
 
-			</div>
-			<div class="col-6">
+		<?php snippet('fields/links',[
+			'links' => $site->links()
+		]); ?>
 
-				<?php if( $site->imprint()->isNotEmpty() ): ?>
-					<a target="_blank" href="<?= $site->imprint() ?>">Imprint</a>
-				<?php endif; ?>
-
-				<?php snippet('fields/links',[
-					'links' => $site->links()
-				]); ?>
-
-			</div>
-
-		</div>
 	</footer>
 
 	<?= js('assets/js/swiper.min.js') ?>
