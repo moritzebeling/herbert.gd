dee3c6ff4a09d71a9a28ff9d2e840bea862a59e9
moritzebeling <git@moritzebeling.com>
2020-04-23 22:19:46 +0200

    minimal info css

--- site/templates/info.php
+++ site/templates/info.php
@@ -12,64 +12,69 @@
       <?= $page->body()->kirbytext(); ?>
     <?php endif ?>
 
-    <?php $team = $page->team()->toStructure();
-    if( $team->count() > 0 ): ?>
-      <div class="team">
-        <ul>
-          <?php foreach( $team as $member ): ?>
-            <li>
+  </section>
+
+  <?php $team = $page->team()->toStructure();
+  if( $team->count() > 0 ): ?>
+    <section class="team">
+      <ul>
+        <?php foreach( $team as $member ): ?>
+          <li>
+            <div>
 
               <?php if( $image = $member->image()->toFile() ): ?>
                 <?= $image->figure(); ?>
               <?php endif; ?>
 
-              <h3><?= $member->name()->html(); ?></h3>
+            </div>
+            <div>
+
+              <h3 class="name"><?= $member->name()->html(); ?></h3>
 
               <?= $member->text()->kirbytext(); ?>
 
               <?= $member->link()->toAnchor(); ?>
 
-            </li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-    <?php endif; ?>
-
-    <?php if( $page->contact()->isNotEmpty() ): ?>
-      <div class="contact">
-        <?= Html::email( $page->contact() ) ?>
-      </div>
-    <?php endif ?>
-
-    <div class="credits">
-      <ul>
-        <li>
-          <div class="job">Webdesign & Programmierung</div>
-          <div class="name">
-            <a href="https://moritzebeling.com">Moritz Ebeling</a>
-          </div>
-        </li>
-        <?php foreach( $page->credits()->toStructure() as $credit ): ?>
-          <li>
-            <div class="job"><?= $credit->job()->html() ?></div>
-            <div class="name">
-              <?php if( $credit->link()->isNotEmpty() ): ?>
-                <?= $credit->link()->toAnchor( $credit->name()->html() ) ?>
-              <?php else: ?>
-                <?= $credit->name(); ?>
-              <?php endif; ?>
             </div>
           </li>
         <?php endforeach; ?>
       </ul>
-    </div>
+    </section>
+  <?php endif; ?>
 
-    <?php if( $page->imprint()->isNotEmpty() ): ?>
-      <div class="imprint">
-        <?= $page->imprint()->toAnchor('Imprint'); ?>
-      </div>
-    <?php endif; ?>
+  <?php if( $page->contact()->isNotEmpty() ): ?>
+    <section class="contact">
+      <?= Html::email( $page->contact() ) ?>
+    </section>
+  <?php endif ?>
 
+  <?php if( $page->imprint()->isNotEmpty() ): ?>
+    <section class="imprint">
+      <?= $page->imprint()->toAnchor('Imprint'); ?>
+    </section>
+  <?php endif; ?>
+
+  <section class="credits">
+    <ul>
+      <li>
+        <div class="job">Webdesign & Programmierung</div>
+        <div class="name">
+          <a href="https://moritzebeling.com">Moritz Ebeling</a>
+        </div>
+      </li>
+      <?php foreach( $page->credits()->toStructure() as $credit ): ?>
+        <li>
+          <div class="job"><?= $credit->job()->html() ?></div>
+          <div class="name">
+            <?php if( $credit->link()->isNotEmpty() ): ?>
+              <?= $credit->link()->toAnchor( $credit->name()->html() ) ?>
+            <?php else: ?>
+              <?= $credit->name(); ?>
+            <?php endif; ?>
+          </div>
+        </li>
+      <?php endforeach; ?>
+    </ul>
   </section>
 
 </main>
