e1ae5ec0a9e15e998e146ce7eddd3c7b8d0eeeec
moritzebeling <git@moritzebeling.com>
2020-05-29 16:25:50 +0200

    working on results-options

--- site/snippets/posts/posts.php
+++ site/snippets/posts/posts.php
@@ -5,7 +5,30 @@
 $dateFormat = $page->dateFormat();
 
 ?>
-<section class="posts list">
+<section class="posts <?= $page->layout() ?>">
+
+	<nav class="results-options">
+
+		<div class="left">
+			<label for="results-layout">Layout</label>
+			<ul name="results-layout" id="results-layout">
+			  <li value="s">Small</li>
+			  <li value="m">Medium</li>
+			  <li value="l">Large</li>
+			</ul>
+		</div>
+
+		<div class="right">
+			<label for="results-filter">Categories</label>
+			<ul name="results-filter" id="results-filter">
+			  <li value="false">All</li>
+				<?php foreach( $posts->pluck('categories', ',', true) as $category ): ?>
+					<li value="<?= $category; ?>"><?= ucwords( $category ); ?></li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+
+	</nav>
 
 	<ol>
 		<?php foreach( $posts as $post ): ?>
