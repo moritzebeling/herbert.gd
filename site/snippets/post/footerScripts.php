85151f848cdd94b50f1e2af11b850ce631fe7851
moritzebeling <git@moritzebeling.com>
2020-06-29 17:08:20 +0200

    include swiper scripts only on single post page

---
+++ site/snippets/post/footerScripts.php
@@ -0,0 +1,89 @@
+<?= js('assets/js/swiper.min.js') ?>
+<script>
+
+	const swiperOptions = {
+		speed: 700,
+		spaceBetween: 200,
+		loop: true,
+		lazy: true,
+		grabCursor: true,
+		initialSlide: 0,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'fraction',
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: true,
+		},
+	};
+
+	const gContainer = document.querySelector('.gallery');
+	let gSwiper, figures;
+
+	if( gContainer && gContainer.classList.contains('multiple') ){
+
+		figures = gContainer.getElementsByTagName('FIGURE');
+		startSwiper();
+
+		gContainer.querySelector( '.swiper-button-index' ).addEventListener( 'click', ()=>{
+			if( gContainer.classList.contains('index') ){
+				startSwiper();
+			} else {
+				showIndex();
+			}
+		}, false );
+
+	}
+
+	function startSwiper( i = 0 ){
+		console.log('show slider', i);
+
+		gContainer.classList.remove('index');
+		gContainer.classList.add('swiper-container');
+
+		for (var figure of figures) {
+			figure.removeEventListener( 'click', clickOnIndex );
+		}
+
+		swiperOptions.initialSlide = i;
+		gSwiper = new Swiper( gContainer, swiperOptions );
+	}
+
+	function showIndex(){
+		console.log('show index');
+
+		gSwiper.destroy();
+		gSwiper = undefined;
+
+		gContainer.classList.remove('swiper-container');
+		gContainer.classList.add('index');
+
+		for (var figure of figures) {
+			figure.addEventListener( 'click', clickOnIndex, false );
+		}
+
+	}
+
+	function whatN(element, type){
+		let i = 0;
+		while( element = element.previousElementSibling ){
+			i++;
+		}
+		return i;
+	}
+
+	function clickOnIndex( event ){
+
+		let slide = event.target.closest('.swiper-slide');
+		let i = whatN( slide );
+
+		startSwiper( i );
+
+	}
+
+</script>
