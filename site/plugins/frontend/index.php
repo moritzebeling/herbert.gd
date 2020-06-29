f5c4b488ab36c80e2f3074ce895baedbfbaeb823
moritzebeling <git@moritzebeling.com>
2020-06-29 16:03:44 +0200

    cache api return data

--- site/plugins/frontend/index.php
+++ site/plugins/frontend/index.php
@@ -7,6 +7,11 @@
 
 Kirby::plugin('herbert/frontend', [
 
+  'options' => [
+    'cache' => true,
+    'expires' => 1440
+  ],
+
   'routes' => [
     [
       'pattern' => '(:all).json',
@@ -14,16 +19,40 @@ Kirby::plugin('herbert/frontend', [
 
         $kirby = kirby();
 
-        if( $page = page( $path ) ){
-          
+        if( $page = kirby()->page( $path ) ){
+
         } else {
           $page = kirby()->site()->homePage();
+        }
+
+        $result = null;
+        $cached = false;
+
+        if( option('herbert.frontend.cache',false) ){
+
+          $cache = $kirby->cache('herbert.frontend');
+          $result = $cache->get( $page->id() );
+
+          if( $result !== null ){
+            $cached = true;
+          }
+
+        }
+
+        if( $result === null ){
+          $result = $page->json();
+
+          if( option('herbert.frontend.cache',false) ){
+            $cache->set( $page->id(), $result, option('herbert.frontend.expires',1440) );
+          }
+
         }
 
         return [
           'status' => 200,
           'request' => $kirby->request()->url()->toString(),
-          'data' => $page->json()
+          'cached' => $cached,
+          'data' => $result
         ];
 
       }
