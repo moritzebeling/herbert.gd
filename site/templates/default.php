5999c04c0e1432e6884290b80246823508ad11c7
moritzebeling <git@moritzebeling.com>
2020-07-14 21:39:28 +0200

    call svelte frontend from all php templates

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,19 +1,46 @@
-<?php snippet('header') ?>
+<!doctype html>
+<html>
+<head>
 
-<main>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <header class="post-header">
-    <h1><?= $page->title()->html() ?></h1>
-  </header>
+  <title>Herbert.gd</title>
 
-  <section class="content">
+  <link rel="canonical" href="<?= $page->url() ?>" />
 
-    <?php if( $page->body()->isNotEmpty() ): ?>
-      <?= $page->body()->kirbytext(); ?>
-    <?php endif ?>
+  <?= css([
+    'assets/css/reset.css',
+    'assets/css/swiper.css',
+    'assets/css/global.css',
+    '@auto'
+  ]) ?>
 
-  </section>
+  <?= js([
+    'assets/js/morutilities.js',
+    'assets/js/default.js'
+  ]) ?>
 
-</main>
+  <meta name="description" content="<?php e( $page->description()->isNotEmpty(), $page->description()->html(), $site->description()->html() ) ?>">
+  <meta name="keywords" content="<?= implode(', ', array_merge( $page->keywords()->split(), $site->keywords()->split() )) ?>">
 
-<?php snippet('footer');
+  <?php
+  $parent = $page->isHomePage() ? $site : $page;
+  if( $image = $parent->content()->image()->toFile() ): ?>
+    <meta property="og:image" content="<?= $image->thumb()->url(['width' => 1000]) ?>" />
+  <?php elseif( $image = $parent->image() ): ?>
+    <meta property="og:image" content="<?= $image->thumb()->url(['width' => 1000]) ?>" />
+  <?php endif ?>
+
+</head>
+<!-- This website was made by Moritz Ebeling https://moritzebeling.com -->
+<!-- If you want to contribute to this website, go to https://github.com/moritzebeling/herbert.gd -->
+<body>
+
+    <div id="frontend"></div>
+
+    <?= js('media/plugins/herbert/frontend/bundle.js'); ?>
+    <?php echo css('media/plugins/herbert/frontend/bundle.css'); ?>
+
+</body>
+</html>
---
+++ site/templates_backup/default.php
@@ -0,0 +1,19 @@
+<?php snippet('header') ?>
+
+<main>
+
+  <header class="post-header">
+    <h1><?= $page->title()->html() ?></h1>
+  </header>
+
+  <section class="content">
+
+    <?php if( $page->body()->isNotEmpty() ): ?>
+      <?= $page->body()->kirbytext(); ?>
+    <?php endif ?>
+
+  </section>
+
+</main>
+
+<?php snippet('footer');
