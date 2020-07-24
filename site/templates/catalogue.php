780974b67efc053b57d9f682c611be46e53c1dd0
moritzebeling <git@moritzebeling.com>
2020-07-24 17:54:43 +0200

    add thumbnails to catalogue

--- site/templates/catalogue.php
+++ site/templates/catalogue.php
@@ -7,13 +7,14 @@
 
 	<?php foreach( $kirby->collection('channels') as $channel ): ?>
         <section>
-            <h2><?= $channel->title(); ?></h2>
+            <h2><a target="_blank" href="<?= $channel->url() ?>"><?= $channel->title(); ?></a></h2>
 
             <table width="100%">
 
                 <tr>
-                    <th>Title</th>
-                    <th>Subtitle</th>
+                    <th class="s">&nbsp;</th>
+                    <th class="l">Title</th>
+                    <th class="l">Subtitle</th>
                     <th>Date</th>
                     <th class="s">Words</th>
                     <th class="s">Images</th>
@@ -25,8 +26,32 @@
 
                 <?php foreach( $channel->posts() as $post ): ?>
                     <tr>
-                        <td><a target="_blank" href="<?= $post->url() ?>"><?= $post->title() ?></a></td>
-                        <td><a target="_blank" href="<?= $post->url() ?>"><?= $post->subtitle() ?></a></td>
+                        <td class="img">
+                            <a target="_blank" href="<?= $post->url() ?>">
+                                <img src="<?= $post->image()->url() ?>" srcset="<?= $post->image()->srcset([
+                                    '1x' => [
+                                        'width' => 40,
+                                        'height' => 40,
+                                        'crop' => 'center'
+                                    ],
+                                    '2x' => [
+                                        'width' => 80,
+                                        'height' => 80,
+                                        'crop' => 'center'
+                                    ]
+                                ]) ?>" />
+                            </a>
+                        </td>
+                        <td class="l">
+                            <a target="_blank" href="<?= $post->url() ?>">
+                                <?= $post->title() ?>
+                            </a>
+                        </td>
+                        <td class="l">
+                            <a target="_blank" href="<?= $post->url() ?>">
+                                <?= $post->subtitle() ?>
+                            </a>
+                        </td>
                         <td><?= $post->displayDate() ?></td>
                         <td class="s"><?= $post->body()->words() ?></td>
                         <td class="s"><?= $post->images()->count() ?></td>
