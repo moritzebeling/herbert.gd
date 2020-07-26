211c1c71629a7d608d15a1bc50284d4921861650
moritzebeling <git@moritzebeling.com>
2020-07-26 15:27:59 +0200

    opengraph and html meta

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,34 +1,55 @@
-<!doctype html>
-<html>
-<head>
+<?php
 
+$metaParent = $page;
+
+if( $page->isHome() ){
+	$metaParent = $site;
+}
+
+if( $metaParent->subtitle()->isNotEmpty() ){
+	$desc = $metaParent->subtitle()->value();
+} elseif ( $metaParent->description()->isNotEmpty() ){
+	$desc = $metaParent->description()->value();
+} else {
+	$desc = $site->description()->value();
+}
+
+$keywords = array_merge( $page->keywords()->split(), $site->keywords()->split() );
+
+if( $image = $metaParent->image() ){
+} elseif( $image = $parent->image() ) {
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-	<title><?= $page->title() ?> • <?= $site->title() ?></title>
-
-	<link rel="canonical" href="<?= $page->url() ?>" />
-
 	<?= css('assets/css/global.css') ?>
 
-	<?php
-	/*
-	insert here:
-		- js libraries async
-		- favicon
-	*/
-	?>
+	<?php if( $page->isHomePage() ): ?>
+		<title><?= $site->title() ?></title>
+	<?php else: ?>
+		<title><?= $page->title() ?> • <?= $site->title() ?></title>
+	<?php endif; ?>
 
-	<meta name="description" content="<?php e( $page->description()->isNotEmpty(), $page->description()->html(), $site->description()->html() ) ?>">
-	<meta name="keywords" content="<?= implode(', ', array_merge( $page->keywords()->split(), $site->keywords()->split() )) ?>">
+	<link rel="canonical" href="<?= $page->url() ?>" />
+	<meta name="description" content="<?= $desc ?>">
+	<meta name="keywords" content="<?= implode( ', ', $keywords ) ?>">
+	<meta name="Generator" content="Moritz Ebeling (https://moritzebeling.com)">
+	<?php if( $info = page('info') ): ?>
+		<link rel="author" href="<?= $info->url() ?>">
+	<?php endif; ?>
 
-	<?php
-	$parent = $page->isHomePage() ? $site : $page;
-	if( $image = $parent->content()->image()->toFile() ): ?>
-		<meta property="og:image" content="<?= $image->thumb()->url(['width' => 1000]) ?>" />
-	<?php elseif( $image = $parent->image() ): ?>
-		<meta property="og:image" content="<?= $image->thumb()->url(['width' => 1000]) ?>" />
-	<?php endif ?>
+	<meta property="og:site_name" content="<?= $site->title() ?>">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="<?= $metaParent->title() ?>">
+	<meta property="og:url" content="<?= $metaParent->url() ?>">
+	<meta property="og:locale" content="de_DE">
+	<meta property="og:description" content="<?= $desc ?>">
+	<meta property="og:image" content="<?= $image->resize(1000,null,80)->url() ?>" />
 
 	<?php snippet('header/favicon') ?>
 
