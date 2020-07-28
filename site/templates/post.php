3a034f5529b6fd456778afdb9b1032dc00a9f8e0
moritzebeling <git@moritzebeling.com>
2020-07-29 01:50:17 +0200

    post content

--- site/templates/post.php
+++ site/templates/post.php
@@ -4,17 +4,24 @@
 
 	<?php snippet('post/gallery') ?>
 
+	<section class="title">
+
+		<h1><?= $page->title()->html() ?></h1>
+		<?php if( $page->subtitle()->isNotEmpty() ): ?>
+			<h2><?= $page->subtitle() ?></h2>
+		<?php endif; ?>
+
+	</section>
+
 	<section class="content">
-		<div class="info">
-
-			<?php snippet('post/title'); ?>
-
-			<?php snippet('post/meta') ?>
-
-		</div>
 		<div class="about">
 
 			<?= $page->body()->kirbytext(); ?>
+
+		</div>
+		<div class="info">
+
+			<?php snippet('post/meta'); ?>
 
 		</div>
 	</section>
