de127ad511040733eebfc891896b71f2a3f94473
moritzebeling <git@moritzebeling.com>
2021-01-28 23:55:32 +0100

    remove kirby pagetable plugin

--- site/plugins/kirby-pagetable/lib/languages/es_419.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-  'pagetable.rowsPerPage'  => 'Página mostradas',
-  'pagetable.of'           => 'de',
-  'pagetable.all'          => 'Todas',
-  'pagetable.filter-pages' => 'Filtrar páginas…',
-  'pagetable.reset'        => 'Restablecer',
-);
